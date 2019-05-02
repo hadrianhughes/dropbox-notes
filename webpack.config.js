932121dbafd7b702984e8b83a135ab1171a3b543
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-05-02 17:35:40 +0100

    Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/index',
+	module: {
+		rules: [
+			{ test: /\.js$/, use: 'babel-loader' }
+		]
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build')
+	},
+	plugins: [
+		new CopyPlugin([ from: 'static' ])
+	]
+};
+
