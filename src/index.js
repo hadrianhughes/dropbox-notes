c06ca401dc431fcd57520f304620c6c468b2986d
Hadrian Hughes <hadrian.hughes@amazerealise.com>
2019-05-03 10:09:32 +0100

    Install webpack-dev-server and bootstrap React

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-console.info('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+ReactDOM.render(<h1>Hello World!</h1>, container);
 
